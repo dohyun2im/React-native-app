ce97aad6dae7e1a4e6ce292f655fabd547aa46c7
dohyun2im <ehehwhdwhd@naver.com>
2023-04-12 20:08:20 +0900

    1차 커밋

--- FootApp_agilegrowth/App.tsx
+++ FootApp_agilegrowth/App.tsx
@@ -8,6 +8,7 @@ import {
 import SignIn from './src/SignIn';
 import SignUp from './src/SignUp';
 import FootSize from './src/FootSize';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export type RootStackParamList = {
@@ -23,27 +24,29 @@ function App() {
           <NavigationContainer>
             <Tab.Navigator>
               <Tab.Screen
-                name="SignIn"
-                component={SignIn}
-                options={{
-                  title: '로그인',
-                  tabBarIcon: () => <FontAwesome5 name="list" size={20} />,
-                }}
-              />
-              <Tab.Screen
                 name="SignUp"
                 component={SignUp}
                 options={{
                   title: '회원가입',
-                  tabBarIcon: () => <FontAwesome5 name="list" size={20} />,
+                  tabBarIcon: () => <MaterialCommunityIcons name="file-sign" size={20} />,
                 }}
               />
+              
+              <Tab.Screen
+                name="SignIn"
+                component={SignIn}
+                options={{
+                  title: '로그인',
+                  tabBarIcon: () => <MaterialCommunityIcons name="login" size={20} />,
+                }}
+              />
+
               <Tab.Screen
                 name="FootSize"
                 component={FootSize}
                 options={{
                   title: '측정',
-                  tabBarIcon: () => <FontAwesome5 name="list" size={20} />,
+                  tabBarIcon: () => <MaterialCommunityIcons name="foot-print" size={20} />,
                 }}
               />
             </Tab.Navigator>
